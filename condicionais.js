4636c30215fe393a17acda49a75bf9f1b9a7164c
Eduardo Siles <eduardosiles2003@gmail.com>
2024-02-02 15:44:42 -0300

    Condicionais

---
+++ condicionais.js
@@ -0,0 +1,16 @@
+let a = 6
+let b = 5
+
+/*
+
+if (a == b) {
+   console.log("São iguais!")
+} else {
+   console.log("São diferentes!")
+}
+
+*/
+
+let ternario = a == b ? "São iguais!" : "São diferentes!"
+
+console.log(ternario)
