95bc875d2d59ce840a4e3ed72ca39e3d6efeca33
Eduardo Siles <eduardosiles2003@gmail.com>
2024-02-02 16:42:24 -0300

    Arrays (reduce)

--- byronJavascript/main.js
+++ byronJavascript/main.js
@@ -1,9 +1,7 @@
 let A = [2, 3, 5, 7, 8, 20]
 
-let C = ["Olá", "Tudo bem", "Como vai", "Tudo certo", "Beleza"]
-
-let teste = A.find((element) => {
-  return element > 5
-})
-
-document.body.innerText = teste // console.log
+let B = A.reduce((anterior, atual) => {
+  return anterior + atual
+}, 1)
+ 
+document.body.innerText = B // console.log
